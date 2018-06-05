6c6c377b70d5e6f2672cc6bd3a88f9fb17f72124
Administrator <Administrator@PC-20170311ZVFQ>
2018-06-05 17:13:54 +0800

    sprint-boot learning

---
+++ src/main/java/example/Example.java
@@ -0,0 +1,22 @@
+package example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(Example.class, args);
+		
+	}
+
+	@RequestMapping("/")
+	String home() {
+		return "Home Page";
+	}
+}
